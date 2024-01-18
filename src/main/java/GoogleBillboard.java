60de2794840e2f1b3a62d013c31692073631c5d5
erikazeng <143123644+erikazeng@users.noreply.github.com>
2024-01-18 12:25:20 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,30 +1,23 @@
-public class GoogleBillboard {
-  public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-  public static void main(final String[] args) {
-    or (int i =0;
-    i<e.length();
-    i++) {
-      String digits = e.substring(2+i, 12+i);
-      double dNum = Double.parseDouble(digits);
-      if (isPrime(dNum) == true) {
-        System.out.println(digits);
-        break;
-      }
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
+public void setup() {
+  for (int i =0; i<e.length(); i++) {
+    String digits = e.substring(2+i, 12+i);
+    double dNum = Double.parseDouble(digits);
+    if (isPrime(dNum) == true) {
+      System.out.println(digits);
+      break;
     }
-  }
-
-
-  //Finish this function
-  public static boolean isPrime(double dNum) {
-    if (dNum<2) {
-      return false;
-    }
-    for (int i =2; i<=Math.sqrt(dNum); i++) {
-      if (dNum%i==0) {
-        return false;
-      }
-    }
-    return true;
   }
 }
-
+public static boolean isPrime(double dNum)
+{
+  if (dNum<2) {
+    return false;
+  }
+  for (int i =2; i<=Math.sqrt(dNum); i++) {
+    if (dNum%i==0) {
+      return false;
+    }
+  }
+  return true;
+}
