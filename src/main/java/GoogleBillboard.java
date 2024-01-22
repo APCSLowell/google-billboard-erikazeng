5b186cdc93d6ced2d523e0f488b553496ffdc657
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-22 14:16:39 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,5 +1,11 @@
+
+public class GoogleBillboard{
+
+public static void main(String[] args){
+  setup();
+}
 public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";
-public void setup() {
+public static void setup() {
   for (int i =0; i<e.length(); i++) {
     String digits = e.substring(2+i, 12+i);
     double dNum = Double.parseDouble(digits);
@@ -20,4 +26,5 @@ public static boolean isPrime(double dNum)
     }
   }
   return true;
+}
 }
